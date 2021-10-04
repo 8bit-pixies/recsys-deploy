8e445121fd74f75899d4eb5789efbf5ff35df4e9
CS <2498638+charliec443@users.noreply.github.com>
2021-10-05 09:28:30 +1100

    adding initial model skeletons using gensim + faiss

---
+++ notebooks/README.md
@@ -0,0 +1,1 @@
+This folder is where we build the models - let's not think too hard to start off with.
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 from setuptools import find_packages
 
-REQUIRED = ["pandas", "scikit-learn", "colorama", "pydantic"]
+REQUIRED = ["pandas", "scikit-learn", "colorama", "pydantic", "colorama"]
 
 DEV_REQUIRED = [
     "black",
